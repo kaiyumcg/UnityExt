faf394c27fc99b35f48c517aee57d17c2781a512
kaiyumPC <kaiyumcg@gmail.com>
2022-11-19 21:10:21 +0600

    game trail for vortex

---
+++ Runtime/Component/GameTrail.cs
@@ -0,0 +1,47 @@
+using AttributeExt;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityExt
+{
+    public class GameTrail : MonoBehaviour
+    {
+        Transform tr;
+        public Transform _Transform { get { return tr; } }
+        [SerializeField, CanNotEdit]TrailRenderer trail;
+        [SerializeField, CanNotEdit] string status;
+        public TrailRenderer Trail { get { return trail; } }
+        public void Init()
+        {
+            tr = transform;
+            trail = GetComponentInChildren<TrailRenderer>();
+            Clear();
+        }
+
+        public void Clear()
+        {
+            if (trail != null)
+            {
+                trail.Clear();
+            }
+            status = "Clear called";
+        }
+        public void Hide()
+        {
+            if (trail != null)
+            {
+                trail.enabled = false;
+            }
+            status = "Hide called";
+        }
+        public void ForceEmiting()
+        {
+            if (trail != null)
+            {
+                trail.emitting = true;
+            }
+            status = "Force emitting called";
+        }
+    }
+}
